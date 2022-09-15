51bfea5d2ce25498ac7539e3705ac80744a46e3a
FOXTetsuo <60584659+FOXTetsuo@users.noreply.github.com>
2022-09-15 11:52:27 +0200

    Commit

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ControllerTest
+{
+	[TestFixture]
+	public class Model_Competition_NextTrackShould
+	{
+		private Competition _competition;
+		public Competition Competition { get { return _competition; } set { _competition = value; } }
+		[SetUp]
+		public void SetUp()
+		{
+			_competition = new Competition();
+		}
+		[Test]
+		public void NextTrack_EmptyQueue_ReturnNull()
+		{
+			Track result = Competition.NextTrack();
+			Assert.IsNull(result);
+		}
+	}
+	
+}
